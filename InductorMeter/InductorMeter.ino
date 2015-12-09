b34d5e24e3a3ad251940bb2998d0cfcfc4997a06
Zaher Dirkey <zaherdirkey@yahoo.com>
2015-12-09 19:35:50 +0200

    [InductorMeter] Add: sum the 10 times then calc the average of pluse, but i dont use it, bad idea

--- InductorMeter/InductorMeter.ino
+++ InductorMeter/InductorMeter.ino
@@ -9,67 +9,106 @@ static int pinIn = 10;
 static int pinLED = 13;
 static double delayPulse = 100; // original is 100
 static int pulseTimeout = 5000;
+//Max of tries to before calc the average
+//static int triesCount = 10.0; 
+static int triesCount = 1.0; //I felt it is not good idea so i will use 1 try
 
-double pulse, frequency, inductance;
+double pulse, avgPulse, frequency, inductance;
+
+int tries = 0;
+
 //insert capacitance here. Currently using 1uF/2uF, change it depend on your capacitors tolerance, I calebrated it
-//double capacitance = 1.79E-6; 
-//double capacitance = 1.03E-6; 
-double capacitance = 1.00E-6; 
+double capacitance = 1.03E-6;
+//double capacitance = 1.00E-6;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(pinIn, INPUT);
   pinMode(pinOut, OUTPUT);
   pinMode(pinLED, OUTPUT);
   digitalWrite(pinLED, LOW);
   delay(200);
-  
+
+  avgPulse = 0;
+
   digitalWrite(pinOut, HIGH);
-  delay(5);//give some time to charge inductor.  
-  digitalWrite(pinOut, LOW);  
+  delay(5);//give some time to charge inductor.
+  digitalWrite(pinOut, LOW);
 }
 
-void loop() {
-  //digitalWrite(pinOut, HIGH);
-  //delay(5);//give some time to charge inductor.
-  //digitalWrite(pinOut, LOW);
-  //delayMicroseconds(delayPulse); //make sure resonation is measured, increase it for over 200uH inductor
-  pulseIn(pinIn, HIGH, pulseTimeout); 
-  pulse = pulseIn(pinIn, LOW, pulseTimeout); 
-  pulse = pulse + pulseIn(pinIn, HIGH, pulseTimeout); //returns 0 if timeout
+void loop()
+{
+  pulseIn(pinIn, HIGH, pulseTimeout);
+  pulse = pulseIn(pinIn, LOW, pulseTimeout);
+  if (pulse >= 0.1) //do not wait the timeout
+    pulse = pulse + pulseIn(pinIn, HIGH, pulseTimeout); //returns 0 if timeout
 
-  if (pulse > 0.1) { //if a timeout did not occur and it took a reading:
-    frequency = 1.E6 / pulse;
-    inductance = 1. / (capacitance * frequency * frequency * 4. * 3.14159 * 3.14159); //one of my profs told me just do squares like this
-    inductance *= 1.E6; //note that this is the same as saying inductance = inductance * 1E6
-
-    Serial.print("Pulse:");
-    Serial.print( pulse );
-    Serial.print("\tFrequency:");
-    Serial.print( frequency );
-    Serial.print("Hz" );
-    Serial.print("\tInductance:");
-    
-    if (inductance >= 1000) {
-      Serial.print( inductance / 1000 );      
-      Serial.println("mH" );
-    }      
-    else {
-      Serial.print(inductance);
-      Serial.println("uH" );
-    }
-    digitalWrite(pinLED, HIGH);
-  }
-  else
-  {
+  if (pulse < 0.1) {
     Serial.print("Pulse:");
     Serial.print( pulse );
     Serial.println(", Insert Inductor");
-    digitalWrite(pinLED, LOW);
-    delay(5);
-    digitalWrite(pinOut, HIGH);
-    delay(5);
-    digitalWrite(pinOut, LOW);      
+    triggerTank();
   }
-  delay(200);
+  else {
+    tries++;
+    /*testing
+    Serial.print("Try ");
+    Serial.print(tries);
+    Serial.print("   ");
+    */
+    avgPulse = avgPulse + pulse;
+    
+    if (tries >= triesCount) {
+      avgPulse = avgPulse / triesCount;
+
+      frequency = 1.E6 / avgPulse;
+      inductance = 1. / (capacitance * frequency * frequency * 4. * 3.14159 * 3.14159); //one of my profs told me just do squares like this
+      inductance *= 1.E6; //note that this is the same as saying inductance = inductance * 1E6
+
+      /*
+         print values
+      */
+      Serial.print("Pulse:");
+      Serial.print( avgPulse );
+      Serial.print("\tFrequency:");
+      Serial.print( frequency );
+      Serial.print("Hz" );
+      Serial.print("\tInductance:");
+      printValue(inductance);
+      if (inductance >= 1000) {
+        Serial.print( inductance / 1000 );
+        Serial.println("mH" );
+      }
+      else {
+        Serial.print(inductance);
+        Serial.println("uH" );
+      }
+      digitalWrite(pinLED, HIGH);
+      delay(500);
+      
+      tries = 0;
+      avgPulse = 0;
+    }
+    else {
+      Serial.print("Pulse:"); //for testing
+      Serial.println( pulse );
+    }
+    delay(50);    
+  }
 }
+
+void printValue(double inductance) {
+  //Code to send it to the display
+}
+
+void triggerTank() {
+  digitalWrite(pinLED, LOW);
+  delay(5);
+  digitalWrite(pinOut, HIGH);
+  delay(5);
+  digitalWrite(pinOut, LOW);
+  delay(5);
+}
+
+
--- KidsKit/TrafficLights/TrafficLights.ino
+++ KidsKit/TrafficLights/TrafficLights.ino
@@ -4,6 +4,8 @@
 
 
 void setup() {
+  pinMode(13, OUTPUT);  
+  digitalWrite(13, LOW); 
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
@@ -14,13 +16,13 @@ void loop() {
   digitalWrite(12, HIGH);    // turn the LED off by making the voltage LOW
   digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(10, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(10000);              // wait for a second
+  delay(3000);              // wait for a second
   digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(11, HIGH);    // turn the LED off by making the voltage LOW
   digitalWrite(10, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);              // wait for a second
+  delay(1000);              // wait for a second
   digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(5000);              // wait for a second
+  delay(2000);              // wait for a second
 }
