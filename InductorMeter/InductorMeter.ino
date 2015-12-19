6699867abb0a64f355c822c0337b412c02264084
Zaher Dirkey <zaherdirkey@yahoo.com>
2015-12-19 15:08:22 +0200

    Changed to use Interrupts using PinChangeInterrupt library

--- InductorMeter/InductorMeter.ino
+++ InductorMeter/InductorMeter.ino
@@ -1,39 +1,39 @@
+#include <PinChangeInterrupt.h>
+
 /**
 * This file is part of the "SARD"
-* 
-* @license   The MIT License (MIT) Included in this distribution            
-* 
-* @based on 
+*
+* @license   The MIT License (MIT) Included in this distribution
+*
+* @based on
 *    http://reibot.org/2011/07/19/measuring-inductance/
-*    http://soundation.blogspot.com/2012/07/arduino-inductance-meter.html   
+*    http://soundation.blogspot.com/2012/07/arduino-inductance-meter.html
+*    http://codinglab.blogspot.be/2013/02/measuring-inductances-using-arduino.html
 *    http://forum.allaboutcircuits.com/threads/inductor-meter-on-arduino.118518/
+*
+*
+* @ref:
+*    http://www.benripley.com/diy/arduino/three-ways-to-read-a-pwm-signal-with-arduino/
 *    
 * @author    Zaher Dirkey <zaherdirkey at yahoo dot com>
-* 
+*
 */
 
 static int pinOut = 9; //is the comparator/op-amp output.
 static int pinIn = 10; //input to the circuit (connects to 150ohm resistor)
 static int pinLED = 13;
-static double delayPulse = 100; // original is 100
-static int pulseTimeout = 5000;
 
-//Max of tries to before calc the average
-//static int triesCount = 10.0;
-static int triesCount = 1.0; //I felt it is not good idea so i will use 1 try
-
-double pulse, avgPulse, frequency, inductance;
-
-int tries = 0;
+double pulse, frequency, inductance;
 
 //insert capacitance here. Currently using 1uF/2uF, change it depend on your capacitors tolerance, I calebrated it
-double capacitance = 1.035E-6;
-//double capacitance = 1.00E-6;
+//double capacitance = 1.035;
+double capacitance = 1.00;
 
 void setup()
-{
+{  
   Serial.begin(115200);
   pinMode(pinIn, INPUT);
+  digitalWrite(pinIn, LOW);
   pinMode(pinOut, OUTPUT);
   pinMode(pinLED, OUTPUT);
   digitalWrite(pinLED, LOW);
@@ -42,56 +42,62 @@ void setup()
   Serial.println("Version: 1.1");
   Serial.println("url: https://github.com/zaher/arduino_projects/");
   Serial.println("---------------------------------------------");
-  delay(200);
 
-  avgPulse = 0;
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(pinIn), tick, FALLING);
+  delay(200);
+  
   triggerTank();
+}
+
+volatile unsigned long pluseCount = 0;
+
+void tick()
+{
+  pluseCount++;
 }
 
 void loop()
 {
-  pulseIn(pinIn, HIGH, pulseTimeout);
-  /**
-   * Now we will take the full length wave time so yes it is HIGH    
-  */
-  pulse = pulseIn(pinIn, LOW, pulseTimeout);
+//  pulseIn(pinIn, HIGH, pulseTimeout);
+//  Now we will take the full length wave time so yes it is HIGH
+
+  /*pulse = pulseIn(pinIn, LOW, pulseTimeout);
   if (pulse >= 0.1) //do not wait if timeouted
     pulse = pulse + pulseIn(pinIn, HIGH, pulseTimeout); //returns 0 if timeout
+*/
+  
+  pluseCount = 0;  
+  delay(1000);
+  frequency = pluseCount;
 
-  if (pulse < 0.1) {
-    Serial.print("Pulse:");
-    Serial.print( pulse );
+  if (frequency < 10) {
+    Serial.print("Frequency: ");    
+    Serial.print(frequency);    
     Serial.println(", Insert Inductor");
-    delay(300);
+    delay(200);
     triggerTank();
+    delay(200);
   }
-  else {
-    tries++;
+  else {    
     /*testing
     Serial.print("Try ");
     Serial.print(tries);
     Serial.print("   ");
     */
-    avgPulse = avgPulse + pulse;
 
-    if (tries >= triesCount) {
-      avgPulse = avgPulse / triesCount;
+//      frequency = 1.E6 / avgPulse;
 
-      frequency = 1.E6 / avgPulse;
-      inductance = 1. / (capacitance * frequency * frequency * 4. * 3.14159 * 3.14159); //one of my profs told me just do squares like this
-      inductance *= 1.E6; //note that this is the same as saying inductance = inductance * 1E6
-
-      //inductance = (avgPulse * avgPulse * 1.E6) / (capacitance *  4. * 3.14159 * 3.14159); //Thanks to Belal al Hamad
+      inductance = (1E6 * 1E6)/ (capacitance * (frequency * frequency) * 4.0 * (PI * PI));
+      //inductance = (1/ (capacitance / (avgPulse * avgPulse) * 4.0 * 3.14159 * 3.14159));
+      //old one inductance = 1.E6 / (capacitance * frequency * frequency * 4. * 3.14159 * 3.14159); //one of my profs told me just do squares like this
 
       /*
          print values
       */
-      Serial.print("Pulse:");
-      Serial.print( avgPulse );
-      Serial.print("\tFrequency:");
+      Serial.print("Frequency: ");
       Serial.print( frequency );
       Serial.print("Hz" );
-      Serial.print("\tInductance:");
+      Serial.print("\tInductance: ");
       printValue(inductance);
       if (inductance >= 1000) {
         Serial.print( inductance / 1000 );
@@ -104,15 +110,7 @@ void loop()
       digitalWrite(pinLED, HIGH);
       delay(500);
 
-      tries = 0;
-      avgPulse = 0;
-    }
-    else {
-      Serial.print("Pulse:"); //for testing
-      Serial.println( pulse );
-    }
-    delay(50);
-  }
+   }
 }
 
 void printValue(double inductance) {
--- InductorMeter/README.md
+++ InductorMeter/README.md
@@ -1,13 +1,15 @@
 # Inductor Meter
 
-This project for Arduino Uno to measure unknown inductors between 50 - 500 uH
+This project for Arduino Uno to measure unknown inductors between 10 - 500 uH
 
 You need to recalibrate your 1uF to get good results.
 Or you can add a bigger size of capacitor like 2uH or more.
 
-	double capacitance = 1.035E-6; 
+	double capacitance = 1.03; 
 
 Do not use polarized capacitor.
+
+It is not stable yet, but it give me a quite good reading.
 
 ![](https://github.com/zaher/arduino_projects/blob/master/InductorMeter/InductorMeter.svg)
 
@@ -17,6 +19,14 @@ http://reibot.org/2011/07/19/measuring-inductance/
 
 http://soundation.blogspot.com/2012/07/arduino-inductance-meter.html
 
+http://codinglab.blogspot.be/2013/02/measuring-inductances-using-arduino.html
+
 http://forum.allaboutcircuits.com/threads/inductor-meter-on-arduino.118518/
 	
 But with some modifications, I made it as permanent oscilator instead of ring by adding a resisitor R3 then in my code I removed the delay before reading the pulses, with mesuring the lenght of full wave not half.
+
+####Library
+
+You need to install this library in your arduino IDE
+ 
+	PinChangeInterrupt
